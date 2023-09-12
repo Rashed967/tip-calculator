a9a407da3176e7db25c7ec01f93425db3268906b
Rashed967 <rashedhasan967.com>
2023-09-12 11:58:54 +0600

    added html code

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const billAmount = document.querySelector("#billAmount"),
+totalPerson = document.querySelector("#persons"),
+tipPercentage = document.querySelector("#tipPercentage"),
+totalBill = document.querySelector("#totalBill"),
+totalTipAmount = document.querySelector("#totalTipAmount");
+
+console.log(billAmount, totalPerson, tipPercentage, totalBill, totalTipAmount,);
