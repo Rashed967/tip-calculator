86b5b0ee1659ba12e20ba24cc1784a8f6ee1afc9
Rashed967 <rashedhasan967@gmail.com>
2023-09-12 23:47:19 +0600

    all are finished

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
 const billAmountElement = document.querySelector("#billAmount"),
 billForm = document.querySelector("#billForm"),
 totalPersonElement = document.querySelector("#persons"),
-tipPercentageElement = document.querySelector("#tipPercentage"),
-totalBillElement = document.querySelector("#totalBill"),
-totalTipAmountElement = document.querySelector("#totalTipAmount");
+tipPercentageElement = document.querySelector("#tipPercentage");
 
 // global variable 
 let billAmount, totalPerson, tipPercentage, totalBillAmount, totalTipAmount;
@@ -14,10 +12,19 @@ billForm.addEventListener('submit', (event) => {
     billAmount = parseInt(billAmountElement.value);
     totalPerson = parseInt(totalPersonElement.value);
     tipPercentage = parseFloat(tipPercentageElement.value);
-    totalBillAmount = billAmount * totalPerson;
-    totalTipAmount = parseFloat(tipPercentage * totalPerson.toFixed(2))
-    console.log(totalBillAmount, totalTipAmount);
 
+    if(isNaN(billAmount) || isNaN(totalPerson) || isNaN(tipPercentage)){
+        return alert("Enter a valild number")
+    }
+    else{
+        totalTipAmount = parseFloat(((billAmount * (tipPercentage / 100)) * totalPerson).toFixed(2))
+        totalBillAmount = parseFloat((billAmount - totalTipAmount).toFixed(2))
+        document.getElementById("totalBill").innerText = `Total Bill: ${totalBillAmount} TK`;
+        document.getElementById("totalTipAmount").innerText = `Total Tip Amount: ${totalTipAmount}`
+        
+    }
+
+    
 
 
 
