b74bbdc54fea2ce9c1eed932c132bc50ad8b143a
Rashed967 <rashedhasan967.com>
2023-09-12 18:42:38 +0600

    added some logic code

--- js/script.js
+++ js/script.js
@@ -1,7 +1,24 @@
-const billAmount = document.querySelector("#billAmount"),
-totalPerson = document.querySelector("#persons"),
-tipPercentage = document.querySelector("#tipPercentage"),
-totalBill = document.querySelector("#totalBill"),
-totalTipAmount = document.querySelector("#totalTipAmount");
+const billAmountElement = document.querySelector("#billAmount"),
+billForm = document.querySelector("#billForm"),
+totalPersonElement = document.querySelector("#persons"),
+tipPercentageElement = document.querySelector("#tipPercentage"),
+totalBillElement = document.querySelector("#totalBill"),
+totalTipAmountElement = document.querySelector("#totalTipAmount");
 
-console.log(billAmount, totalPerson, tipPercentage, totalBill, totalTipAmount,);
+// global variable 
+let billAmount, totalPerson, tipPercentage, totalBillAmount, totalTipAmount;
+
+
+billForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    billAmount = parseInt(billAmountElement.value);
+    totalPerson = parseInt(totalPersonElement.value);
+    tipPercentage = parseFloat(tipPercentageElement.value);
+    totalBillAmount = billAmount * totalPerson;
+    totalTipAmount = parseFloat(tipPercentage * totalPerson.toFixed(2))
+    console.log(totalBillAmount, totalTipAmount);
+
+
+
+
+})
